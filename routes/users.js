8001030a6f175eb1f5eb6cf58b59d4d4e1b61626
Duc Nguyen <ducntq@gmail.com>
2025-10-07 11:16:28 +0700

    Update demo data for users

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,38 @@
 const express = require("express");
 const router = express.Router();
 
+const demoData = [
+  { id: 1, name: "John Doe" },
+  { id: 2, name: "Jane Smith" },
+  { id: 3, name: "Alice Johnson" },
+];
+
 router.get("/", (req, res) => {
   res.json({
+    success: true,
+    errors: [],
     data: {
-      users: [
-        { id: 1, name: "John Doe" },
-        { id: 2, name: "Jane Smith" },
-        { id: 3, name: "Alice Johnson" },
-      ],
+      users: demoData,
     },
   });
 });
 
 router.get("/:id", (req, res) => {
-  res.json({ message: `Get user with id ${req.params.id}` });
-});
-
-router.post("/", (req, res) => {
-  res.json({ message: "Create a new user" });
+  if (isNaN(req.params.id)) {
+    return res
+      .status(400)
+      .json({ success: false, errors: ["Invalid user ID"] });
+  }
+  if (req.params.id < 1 || req.params.id > demoData.length) {
+    return res.status(404).json({ succes: false, errors: ["User not found"] });
+  }
+  res.json({
+    success: true,
+    errors: [],
+    data: {
+      user: demoData[req.params.id - 1],
+    },
+  });
 });
 
 module.exports = router;
